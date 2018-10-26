c948688ef8fa4fb8290b43aa9805e144dc7ce78d
Yonny Vizcaya <YonnyVizcaya@macbook.local>
2018-10-25 19:37:40 -0500

    [Home Modues] Updated

--- src/app/presentation/home/home.module.ts
+++ src/app/presentation/home/home.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   imports: [
+    SharedModule,
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
   ],
-  declarations: []
+  declarations: [IndexComponent]
 })
 export class HomeModule { }

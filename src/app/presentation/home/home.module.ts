7bde630ac7dc2defdcb7e99bfd27d9da6dd37621
yonny Vizcaya <yonnyvizcaya@yonnys-MacBook-Pro.local>
2019-08-19 15:30:05 -0500

    updateing

--- src/app/presentation/home/home.module.ts
+++ src/app/presentation/home/home.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { SharedModule } from '../shared/shared.module';
-
-import { IndexComponent } from './index/index.component';
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { NewsComponent } from './components/news/news.component';
-
 @NgModule({
   imports: [
-    SharedModule,
-    CommonModule,
-    HomeRoutingModule,
+    CommonModule
   ],
-  declarations: [IndexComponent, JumbotronComponent, NewsComponent]
+  declarations: []
 })
 export class HomeModule { }

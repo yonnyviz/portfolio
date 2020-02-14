728e0977d101fadfa0c6c39dd0eeb6c1910e0bf0
Yonny Vizcaya <yonnyviz@Yonnys-MacBook-Pro.local>
2020-02-14 17:17:26 -0500

    [Home] - Updating Styles and Layout

--- src/app/presentation/home/components/home-section/home-section.component.ts
+++ src/app/presentation/home/components/home-section/home-section.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home-section',
+  selector: 'home-section',
   templateUrl: './home-section.component.html',
   styleUrls: ['./home-section.component.scss']
 })
 export class HomeSectionComponent implements OnInit {
-
+  homeInfo: any;
+  imgSrc: any;
   constructor() { }
 
   ngOnInit() {
+    this.homeInfo = this.showHomeInformation();
+    this.imgSrc = this.showImage();
+  }
+
+  private showHomeInformation() {
+    return {
+      brand: "Yonny Vizcaya",
+      title: "Me motiva crear soluciones digitales que impacten positivamente nuestro futuro.",
+      post: "Hola soy Yonny, desarrollador Front-End de applicaciones web, actualmente Desarrollador Front-End en Waki."
+    }
+  }
+
+  private showImage() {
+    return "../../../../../assets/img/selfie2019_bw.JPG"
   }
 
 }

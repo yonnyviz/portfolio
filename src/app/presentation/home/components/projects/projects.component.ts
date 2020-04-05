4e7604c04a5922c8f32338d5ba3c61997e9b8dd3
Yonny Vizcaya <yonnyviz@Yonnys-MacBook-Pro.local>
2020-04-05 12:52:56 -0500

    [HotFix] - Fixing Waki Logo Url

--- src/app/presentation/home/components/projects/projects.component.ts
+++ src/app/presentation/home/components/projects/projects.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+  imgSrc: any;
   constructor() { }
 
   ngOnInit() {
+    this.imgSrc = this.showImage();
+  }
+
+  private showImage() {
+    return "assets/images/waki-logo.png"
   }
 
 }
